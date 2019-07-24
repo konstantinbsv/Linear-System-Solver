1f0d0800b79674a6b384990ad4d9ab8adf342da8
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-24 12:34:10 -0700

    Read program arguments, input file, and populate matrix

--- Linear Equations Solver (Java)/task/src/solver/Main.java
+++ Linear Equations Solver (Java)/task/src/solver/Main.java
@@ -6,5 +6,17 @@ class Main {
 
     public static void main(String[] args) {
 
+        if (args.length != 4 || !"-in".equals(args[0]) && !"-out".equals(args[2])) {
+            System.out.println("Syntax: -in <inputfile.txt> -out <outputfile.txt>");
+            System.exit(-1);
+        }
+
+        File inputFile = new File(args[1]);
+        System.out.println(inputFile);
+
+        Matrix system = new Matrix(inputFile);
+        system.print();
+
+
     }
 }
