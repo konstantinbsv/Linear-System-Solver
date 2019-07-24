86cae00f7a1b160f73528fad1b981c4bb5dba769
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-24 11:07:34 -0700

    Add Matrix constructor

--- Linear Equations Solver (Java)/task/src/solver/Matrix.java
+++ Linear Equations Solver (Java)/task/src/solver/Matrix.java
@@ -1,4 +1,37 @@
 package solver;
 
-public class Matrix {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class Matrix {
+    private int matrixSize;
+    private LinearEquation[] matrix;
+
+    public Matrix(File inputFile) {
+        try (Scanner scanner = new Scanner(inputFile)) {
+            matrixSize = scanner.nextInt(); // first line of file is matrix size
+            if (matrixSize < 1) {
+                throw new IndexOutOfBoundsException("Invalid matrix size: " + matrixSize);
+            }
+
+            matrix = new LinearEquation[matrixSize];
+
+            for (int i = 0; i < matrixSize; i++) {
+                matrix[i] = new LinearEquation(matrixSize); // initialize LE
+                matrix[i].fillEquationFromFile(inputFile);  // fill from file
+            }
+
+        } catch (FileNotFoundException fileNotFound) {
+            System.out.println("File not found: " + inputFile);
+        }
+    }
+
+    public int getMatrixSize() {
+        return matrixSize;
+    }
+
+    public LinearEquation getRow(int row) {
+        return matrix[row];
+    }
 }
