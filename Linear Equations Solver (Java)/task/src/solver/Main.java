6dd1d6b256455e146f6c4d6b40123d0f7b9b8c5a
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-24 15:00:29 -0700

    Write code for REF Gaussian Elimination in Main

--- Linear Equations Solver (Java)/task/src/solver/Main.java
+++ Linear Equations Solver (Java)/task/src/solver/Main.java
@@ -17,6 +17,28 @@ class Main {
         Matrix system = new Matrix(inputFile);
         system.print();
 
+        // Get Row Echelon Form through Gaussian elimination
+        for (int pivot = 1; pivot <= system.getMatrixSize(); pivot++) {
+
+            // multiply pivot by factor that makes it =1
+            double rowFactor = 1/system.getTerm(pivot, pivot);
+            system.getRow(pivot).multiplyRowAndSave(rowFactor);
+            System.out.print(rowFactor <0 ? "-":" "); // neater formatting
+            System.out.printf("%.2f * R%d -> R%d\n", Math.abs(rowFactor), pivot, pivot);
+
+
+            // Perform row ops to get all terms below it =0
+            for (int i = pivot+1; i <= system.getMatrixSize(); i++) {
+                double factor = -system.getTerm(i, pivot)/system.getTerm(pivot, pivot);
+
+                System.out.print(factor <0 ? "-":" "); // neater formatting
+                System.out.printf("%.2f * R%d + R%d -> R%d\n", Math.abs(factor), pivot, pivot+1, pivot+1);
+                system.getRow(i).addToRow(system.getRow(pivot).multiplyRowTemp(factor));
+            }
+        }
+
+        System.out.println();
+        system.print();
 
     }
 }
