1466d854513f31cc877cfc2b92d1cca7f8008db6
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2019-07-31 10:49:30 -0700

    Update LinearEquation to work with ComplexNumber

--- Linear Equations Solver (Java)/task/src/solver/LinearEquation.java
+++ Linear Equations Solver (Java)/task/src/solver/LinearEquation.java
@@ -6,19 +6,19 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class LinearEquation {
-    private double[] equation;
+    private ComplexNumber[] equation;
     private int equationLength;
 
     public LinearEquation(int numOfCoeffs) {
         equationLength = numOfCoeffs + 1; // +1 for constant
-        equation = new double[equationLength];
+        equation = new ComplexNumber[equationLength];
     }
 
-    public double[] getEquation() {
+    public ComplexNumber[] getEquation() {
         return equation;
     }
 
-    public double getTerm(int i) {
+    public ComplexNumber getTerm(int i) {
         if (i < 1 || i > equationLength) {
             throw new IndexOutOfBoundsException("Invalid term index: " + i);
         }
@@ -26,7 +26,7 @@ class LinearEquation {
         return equation[i-1];
     }
 
-    protected void setCoeff(int i, double newTerm) {
+    protected void setCoeff(int i, ComplexNumber newTerm) {
         if (i < 1 || i > equationLength-1) { // can't set constants
             throw new IndexOutOfBoundsException("Invalid term index: " + i);
         }
@@ -34,22 +34,22 @@ class LinearEquation {
         equation[i-1] = newTerm;
     }
 
-    public double getConstantTerm() {
+    public ComplexNumber getConstantTerm() {
         return equation[equationLength-1];
     }
 
-    public void multiplyRowAndSave(double x) {
-        if (x != 1) {
+    public void multiplyRowAndChange(ComplexNumber x) {
+        //if (x != 1) {
             for (int i = 0; i < equationLength; i++) {
-                equation[i] *= x;
-            }
+                equation[i] = equation[i].multiplyBy(x);
+            //}
         }
     }
 
-    public double[] multiplyRowTemp(double x) {
-        double[] result = new double[equationLength];
+    public ComplexNumber[] multiplyRowTemp(ComplexNumber x) {
+        ComplexNumber[] result = new ComplexNumber[equationLength];
         for (int i = 0; i < equationLength; i++) {
-            result[i] = equation[i] *x;
+            result[i] = equation[i].multiplyBy(x);
         }
 
         return result;
@@ -61,10 +61,11 @@ class LinearEquation {
         }
 
         for (int i = 0; i < equationLength; i++) {
-            equation[i] += secondEquation.getTerm(i);
+            equation[i] = equation[i].add(secondEquation.getTerm(i));
         }
     }
 
+    /*
     public void addToRow(double[] secondEquation) {
         if (secondEquation.length != equationLength) {
             throw new InputMismatchException("Linear equations are of unequal size");
@@ -75,29 +76,32 @@ class LinearEquation {
         }
     }
 
+     */
+
     public void subFromRow(LinearEquation secondEquation) {
         for (int i = 0; i < equationLength; i++) {
-            equation[i] -= secondEquation.getTerm(i);
+            equation[i] = equation[i].subtract(secondEquation.getTerm(i));
         }
     }
 
     public void fillEquationFromScanner(Scanner scanner) {
         int i = 0;
         while (scanner.hasNext() && i < equationLength) {
-            equation[i] = scanner.nextDouble();
+            equation[i] = new ComplexNumber(scanner.next());
             i++;
         }
     }
 
     public void print() {
         for (int i = 0; i < equationLength; i++) {
-            System.out.print(equation[i] < 0 ? "-":" "); // neater formatting
-            System.out.printf("%.1f ", Math.abs(equation[i]));
+            // System.out.print(equation[i] < 0 ? "-":" "); // neater formatting
+            // System.out.printf("%.1f ", Math.abs(equation[i]));
+            System.out.println(equation[i].toString());
         }
     }
 
     public boolean isInconsistent() {
-        if (getConstantTerm() == 0) { // e.g. 1 0 4 | 0
+        if (getConstantTerm().isZero()) { // e.g. 1 0 4 | 0
             return false;
         } else {
             if(hasNonZeroCoeffs()) { // e.g. 0 3 0 | 5
@@ -109,7 +113,7 @@ class LinearEquation {
 
     public boolean hasNonZeroCoeffs() {
         for (int i = 0; i < equationLength-1; i++) { // equationLength-1, do not include constant term
-            if (equation[i] != 0) {
+            if (!equation[i].isZero()) {
                 return true;
             }
         }
